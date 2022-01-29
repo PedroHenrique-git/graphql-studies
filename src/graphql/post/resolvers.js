b825404d4563aee1f64a6054e21644e50a656d73
PedroHenrique-git <pedromf34@outlook.com>
2022-01-29 20:01:43 -0300

    union types

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -2,7 +2,16 @@ export const postResolver = {
   Query: {
     post: async (_, { id }, { getPosts }) => {
       const post = await getPosts(`/${id}`);
-      return await post.json();
+      const postData = await post.json();
+
+      if (typeof postData.id === 'undefined') {
+        return {
+          statusCode: 404,
+          message: 'Post not found!',
+        };
+      }
+
+      return postData;
     },
     posts: async (_, { inputFilter }, { getPosts }) => {
       const apiFiltersInput = new URLSearchParams(inputFilter).toString();
@@ -14,6 +23,13 @@ export const postResolver = {
     unixTimeStamp: ({ createdAt }) => {
       const dateInSeconds = Math.floor(new Date(createdAt).getTime() / 1000);
       return dateInSeconds;
+    },
+  },
+  PostResult: {
+    __resolveType: (obj) => {
+      if (typeof obj.statusCode !== 'undefined') return 'PostNotFoundError';
+      if (typeof obj.id !== 'undefined') return 'Post';
+      return null;
     },
   },
 };
