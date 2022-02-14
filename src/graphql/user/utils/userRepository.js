b8281469b42792fbd37815bff4861a628c4992c3
PedroHenrique-git <pedromf34@outlook.com>
2022-02-13 22:43:45 -0300

    add password field

--- src/graphql/user/utils/userRepository.js
+++ src/graphql/user/utils/userRepository.js
@@ -1,17 +1,21 @@
-import { ValidationError } from 'apollo-server';
+import { UserInputError, ValidationError } from 'apollo-server';
+import bcrypt from 'bcrypt';
 
 export const createUserFn = async (userData, dataSource) => {
   const userInfo = await createUserInfo(userData, dataSource);
-  const { firstName, lastName, userName } = userInfo;
+  const { firstName, lastName, userName, password } = userInfo;
 
   if (!isNameValid(firstName, lastName)) {
     throw new ValidationError('First name or last name invalid');
   }
 
-  if (!firstName || !lastName || !userName)
+  if (!firstName || !lastName || !userName || !password)
     throw new ValidationError(
-      'You have to send firstName, lastName and userName',
+      'You have to send firstName, lastName, userName and password',
     );
+
+  validateUserPassword(password);
+  await createPasswordHash(userInfo);
 
   return dataSource.post('', userInfo);
 };
@@ -19,7 +23,7 @@ export const createUserFn = async (userData, dataSource) => {
 export const updateUserFn = async (userId, userData, dataSource) => {
   if (!userId) throw new ValidationError('Missing userId');
 
-  const { firstName, lastName, userName } = userData;
+  const { firstName, lastName, userName, password } = userData;
   const userAlreadyExists = await userExists(userName, dataSource);
 
   if (userAlreadyExists) throw new ValidationError('User already exists');
@@ -27,6 +31,12 @@ export const updateUserFn = async (userId, userData, dataSource) => {
   validateField(firstName, 'firstName');
   validateField(lastName, 'lastName');
   validateField(userName, 'userName');
+  validateField(password, 'password');
+
+  if (typeof password !== 'undefined') {
+    validateUserPassword(password);
+    await createPasswordHash(userData);
+  }
 
   if (!isNameValid(firstName, lastName)) {
     throw new ValidationError('First name or last name invalid');
@@ -82,4 +92,22 @@ export const userExists = async (userName, dataSource) => {
 const validateField = (fieldValue, fieldName) => {
   if (typeof fieldValue !== 'undefined' && !fieldValue)
     throw new ValidationError(`Missing field ${fieldName}`);
+};
+
+const validateUserPassword = (password) => {
+  const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,30}$/;
+
+  if (!password.match(strongPasswordRegex)) {
+    throw new UserInputError('Password invalid');
+  }
+};
+
+const createPasswordHash = async (user) => {
+  if (user.password && !user.passwordHash) {
+    const { password } = user;
+    const passwordHash = await bcrypt.hash(password, 13);
+    user.passwordHash = passwordHash;
+    delete user['password'];
+  }
+  return;
 };
